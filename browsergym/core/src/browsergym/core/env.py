5925d921a3c7c638ab2a8865d3192402865d2582
rutger <rutgercappendijk@gmail.com>
2024-10-08 17:52:36 +0200

    broke axt generation for test

--- browsergym/core/src/browsergym/core/env.py
+++ browsergym/core/src/browsergym/core/env.py
@@ -529,7 +529,7 @@ document.addEventListener("visibilitychange", () => {
             "url": self.page.url,
             "screenshot": extract_screenshot(self.page),
             "dom_object": dom,
-            "axtree_object": axtree,
+            "axtree_object": "this is an AXT",
             "extra_element_properties": extra_properties,
             "focused_element_bid": focused_element_bid,
             "last_action": self.last_action,
