770788f479405c6637f150072d5ba39cfa07d0b3
rutger <rutgercappendijk@gmail.com>
2024-10-09 18:15:03 +0200

    fixed import

--- browsergym/setup.py
+++ browsergym/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     install_requires=[
         "browsergym-workarena @ git+ssh://git@github.com/double-labs/WorkArena.git",
-        "browsergym-core @ git+ssh://git@github.com/double-labs/BrowserGym.git@main#subdirectory=browsergym",
+        "browsergym-core @ git+ssh://git@github.com/double-labs/BrowserGym.git@main#subdirectory=browsergym/core",
         "browsergym-miniwob==0.7.1",
         "browsergym-webarena==0.7.1",
         "browsergym-visualwebarena==0.7.1",
